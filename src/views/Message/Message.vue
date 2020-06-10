328ccac86da7615e8f937409778c9d2e9345080c
Accesslly <541124324@qq.com>
2020-06-10 15:01:10 +0800

    待办界面

--- src/views/Message/Message.vue
+++ src/views/Message/Message.vue
@@ -58,7 +58,82 @@
         </div>
     </div>
         </van-tab>
-        <van-tab title="待办">内容 2</van-tab>
+        <van-tab title="待办">
+             <!-- 活动        -->
+    <div class="activity">
+        <p class="activity-title">活动</p>
+        <div class="a">
+        <div class="audit">
+            <div class="audit-logo">
+                <img class="audit-image" src="../../assets/image/审核.png" alt="报名审核中"><br>
+            </div>
+            <div class="audit-title">
+                <p>报名审核中</p>
+            </div>
+        </div>
+        <div class="ready">
+            <div class="ready-logo">
+                <img class="ready-image" src="../../assets/image/进入.png" alt="待签到"><br>
+            </div>
+            <div class="ready-title">
+                <p>待签到</p>
+            </div>
+        </div>
+        <div class="away">
+            <div class="away-logo">
+                <img class="away-image" src="../../assets/image/离开.png" alt="待签退"><br>
+            </div>
+            <div class="away-title">
+                <p>待签退</p>
+            </div>
+        </div>
+        <div class="evaluation">
+            <div class="evaluation-logo">
+                <img class="evaluation-image" src="../../assets/image/评价记录.png" alt="活动评价"><br>
+            </div>
+            <div class="evaluation-title">
+                <p>活动评价</p>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    <!-- 申请 -->
+    <div class="application">
+        <p class="application-title">申请</p>
+        <div class="audit">
+            <div class="audit-logo">
+                <img class="audit-image" src="../../assets/image/课程报名.png" alt="申请审核中"><br>
+            </div>
+            <div class="audit-title">
+                <p>申请审核中</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- 部落 -->
+    <div class="tribe">
+        <p class="tribe-title">部落</p>
+        <div class="a">
+            <div class="join">
+            <div class="join-logo">
+                <img class="join-image" src="../../assets/image/加入.png" alt="申请加入"><br>
+            </div>
+            <div class="join-title">
+                <p>申请加入</p>
+            </div>
+        </div>
+        <div class="allow">
+            <div class="allow-logo">
+                <img class="allow-image" src="../../assets/image/准入.png" alt="准入审核"><br>
+            </div>
+            <div class="allow-title">
+                <p>准入审核</p>
+            </div>
+        </div>
+        </div>
+    </div>
+        </van-tab>
         <van-tab title="动态">内容 3</van-tab>
     </van-tabs>
     
@@ -240,5 +315,160 @@ export default {
     margin-top: 3px;
     margin-left: 145px;
     color: #bfbfbf;
+}
+.activity{
+    width: 330px;
+    height: 90px;
+    margin-left: 15px;
+    box-shadow:rgba(0,0,0,.1) 0px 0px 2px;
+}
+.activity-title{
+    font-weight: bold;
+    margin-left: -280px;
+}
+.a{
+    display: flex;
+}
+.audit{
+    width: 50px;
+    height: 60px;
+    margin-top: -8px;
+    margin-left: 3px;
+}
+.audit-logo{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 4px;
+    background-color:#f9eced;
+}
+.audit-image{
+    margin-top: 11px;
+}
+.audit-title{
+    font-size: 1px;
+    margin-top: 5px;
+}
+.ready{
+    width: 50px;
+    height: 60px;
+    margin-top: -8px;
+    margin-left: 40px;
+}
+.ready-logo{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 4px;
+    background-color:#faf8dc;
+}
+.ready-image{
+    margin-top: 11px;
+}
+.ready-title{
+    font-size: 1px;
+    margin-top: 5px;
+}
+.away{
+    width: 50px;
+    height: 60px;
+    margin-top: -8px;
+    margin-left: 40px;
+}
+.away-logo{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 4px;
+    background-color:#f8e0eb;
+}
+.away-image{
+    margin-top: 11px;
+}
+.away-title{
+    font-size: 1px;
+    margin-top: 5px;
+}
+.evaluation{
+    width: 50px;
+    height: 60px;
+    margin-top: -8px;
+    margin-left: 40px;
+}
+.evaluation-logo{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 4px;
+    background-color:#cae9f9;
+}
+.evaluation-image{
+    margin-top: 11px;
+}
+.evaluation-title{
+    font-size: 1px;
+    margin-top: 5px;
+}
+.application{
+    width: 330px;
+    height: 90px;
+    margin-top: 13px;
+    margin-left: 15px;
+    box-shadow:rgba(0,0,0,.1) 0px 0px 2px;
+}
+.application-title{
+    font-weight: bold;
+    margin-left: -280px;
+}
+.tribe{
+    width: 330px;
+    height: 90px;
+    margin-top: 13px;
+    margin-left: 15px;
+    box-shadow:rgba(0,0,0,.1) 0px 0px 2px;
+}
+.tribe-title{
+    font-weight: bold;
+    margin-left: -280px;
+}
+.join{
+    width: 50px;
+    height: 60px;
+    margin-top: -8px;
+    margin-left: 3px;
+}
+.join-logo{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 4px;
+    background-color:#f9eced;
+}
+.join-image{
+    margin-top: 11px;
+}
+.join-title{
+    font-size: 1px;
+    margin-top: 5px;
+}
+.allow{
+    width: 50px;
+    height: 60px;
+    margin-top: -8px;
+    margin-left: 40px;
+}
+.allow-logo{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 4px;
+    background-color:#faf8dc;
+}
+.allow-image{
+    margin-top: 11px;
+}
+.allow-title{
+    font-size: 1px;
+    margin-top: 5px;
 }
 </style>
